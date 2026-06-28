b69717271f382f6e8d943009b014867bcda2f16c
GitGinocchio <106624847+GitGinocchio@users.noreply.github.com>
2026-06-28 16:06:13 +0200

    feat: idk some improvement i think

--- scripts/terminal.gd
+++ scripts/terminal.gd
@@ -1,12 +1,5 @@
-extends Node
-
-@onready var animation = %AnimationPlayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+extends CodeEdit
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	print(event)
