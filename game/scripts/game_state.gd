a6b4521ec11b20eb4aa3ab26c4b654bf97ad28ad
GitGinocchio <106624847+GitGinocchio@users.noreply.github.com>
2026-07-20 23:44:27 +0200

    refactor: removed old

--- game/scripts/game_state.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal player_stood_up
-var is_at_pc: bool = true
-
-func _ready() -> void:
-	GameState.player_stood_up.connect(func(): is_at_pc = false)
--- web/app/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <GameCanvas />
-</template>
-
-<script setup lang="ts">
-import GameCanvas from '~/components/GameCanvas.vue'
-</script>
